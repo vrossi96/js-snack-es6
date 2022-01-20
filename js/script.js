8f61a041c7aa4d9de96880a6b1cb59f32f7dab26
vrossi96 <vrossi396@yahoo.it>
2022-01-20 15:33:23 +0100

    add new  values in array

--- js/script.js
+++ js/script.js
@@ -4,15 +4,6 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare in console la bici con peso minore utilizzando destructuring e template literal
 
-
-Snack2
-Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà:
-Punti fatti e falli subiti.
-Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-
-
 BONUS
 Stampare in pagina oltre che in console!
 */
@@ -31,7 +22,6 @@ const biciclette = [
    {nome: 'Lolella', peso: 0},
    {nome: 'Marachella', peso: 0},
 ];
-
 // console.table(biciclette);
 
 for (let i = 0; i < biciclette.length; i++) {
@@ -42,10 +32,9 @@ for (let i = 0; i < biciclette.length; i++) {
       }
    }
 }
-
 console.table(biciclette);
 
-// Creo Array vuoto
+// Creo Array vuoto per i pesi
 const pesi = [];
 for (let i = 0; i < biciclette.length; i++){
    const bicicletta = biciclette[i];
@@ -58,3 +47,42 @@ console.log('Peso minore:', minWeight, 'KG');
 
 
 //# ESERCIZIO 2
+/* 
+Snack2
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà:
+Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. 
+
+BONUS
+Stampare in pagina oltre che in console!
+*/
+
+// Creo array di squadre di calcio
+const teams = [
+   { teamName: 'AC Barbabietola', points: 0, foulAgainst: 0, },
+   { teamName: 'AC Barbabietola', points: 0, foulAgainst: 0, },
+   { teamName: 'AC Barbabietola', points: 0, foulAgainst: 0, },
+   { teamName: 'AC Barbabietola', points: 0, foulAgainst: 0, },
+   { teamName: 'AC Barbabietola', points: 0, foulAgainst: 0, },
+];
+// console.table(teams);
+
+// Inserisco i valori degli oggetti nell array
+for (let i = 0; i < teams.length; i++) {
+   const team = teams[i];
+   for (let key in team) {
+      if (key == 'teamName'){
+         team.teamName = prompt(`Inserisci il nome della ${i +1} squadra:`);
+      }
+      if (key == 'points'){
+         team.points = getRandomNumber(1, 45);
+      }
+      if (key == 'foulAgainst'){
+         team.foulAgainst = getRandomNumber(1, 19);
+      }
+   }
+}
+
+console.table(teams);
