622783345b75bb9c0050e80b55be3beec0d7bde0
vrossi96 <vrossi396@yahoo.it>
2022-01-20 15:20:17 +0100

    add random weight for snack 1

--- js/script.js
+++ js/script.js
@@ -16,18 +16,34 @@ Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengo
 BONUS
 Stampare in pagina oltre che in console!
 */
+// FUNCTIONS
 const getRandomNumber = (min, max) => {
    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
    return randomNumber;
 }
+
+
 //# ESERCIZIO 1
 // Creo array di biciclette (oggetti)
 const biciclette = [
-   {nome: 'Graziella', peso: 30},
-   {nome: 'Marinella',peso: 33},
-   {nome: 'Lolella',peso: 16},
-   {nome: 'Marachella',peso: 28},
+   {nome: 'Graziella', peso: 0},
+   {nome: 'Marinella', peso: 0},
+   {nome: 'Lolella', peso: 0},
+   {nome: 'Marachella', peso: 0},
 ];
+
+// console.table(biciclette);
+
+for (let i = 0; i < biciclette.length; i++) {
+   const bicicletta = biciclette[i];
+   for (let key in bicicletta) {
+      if (key == 'peso'){
+         bicicletta.peso = getRandomNumber(3, 39);
+      }
+   }
+}
+
+console.table(biciclette);
 
 // Creo Array vuoto
 const pesi = [];
@@ -38,13 +54,7 @@ for (let i = 0; i < biciclette.length; i++){
 }
 // Math min prende il numero più basso nell'array
 const minWeight = Math.min(...pesi);
-console.log(minWeight);
-
-
-
-
-
-
+console.log('Peso minore:', minWeight, 'KG');
 
 
 //# ESERCIZIO 2
