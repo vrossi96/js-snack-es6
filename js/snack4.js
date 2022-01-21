96e0b1bf75eadd8c4722f8c9e7a6c53dbe0fce98
vrossi96 <vrossi396@yahoo.it>
2022-01-21 19:05:20 +0100

    add bonus snack 4

--- js/snack4.js
+++ js/snack4.js
@@ -10,17 +10,17 @@ ES.: (marco de iulio  => Marco De Iulio)
 
 const studentClass = [
    {
-      name: "Pero",
+      name: "Pero caduto",
       id: "104",
       sumOfVotes: "88",
    },
    {
-      name: "Pierino",
+      name: "Pierino maestra",
       id: "95",
       sumOfVotes: "69",
    },
    {
-      name: "Tesla",
+      name: "Tesla gas",
       id: "136",
       sumOfVotes: "75",
    },
@@ -30,32 +30,32 @@ const studentClass = [
       sumOfVotes: "88",
    },
    {
-      name: "Gatto",
+      name: "cipro",
       id: "126",
       sumOfVotes: "50",
    },
    {
-      name: "Mclaren",
+      name: "Mclaren cope",
       id: "130",
       sumOfVotes: "92",
    },
    {
-      name: "Luigi",
+      name: "Luigi bros",
       id: "118",
       sumOfVotes: "83",
    },
    {
-      name: "Ned",
+      name: "Ned Needed",
       id: "55",
       sumOfVotes: "65",
    },
    {
-      name: "Nate",
+      name: "Nate jones",
       id: "196",
       sumOfVotes: "96",
    },
    {
-      name: "Mario",
+      name: "Mario bros",
       id: "190",
       sumOfVotes: "64",
    },
@@ -63,10 +63,26 @@ const studentClass = [
 
 const higherVotes = studentClass.filter(({ sumOfVotes }) => sumOfVotes > 70);
 
-console.table(higherVotes);
+// console.table(higherVotes);
 
 const higVotesAndId = studentClass.filter(
    ({ sumOfVotes, id }) => sumOfVotes > 70 && id > 120
 );
 
-console.table(higVotesAndId);
+// console.table(higVotesAndId);
+
+// .charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+
+const nameStudents = studentClass.map(({ name }) => {
+   const word = name.split(" ");
+   console.log(word);
+   let moreWords = "";
+   for (let i = 0; i < word.length; i++) {
+      let words =
+         word[i].charAt(0).toUpperCase() + word[i].slice(1).toLowerCase();
+      // return word[].toUpperCase() + name.substring(1);
+      moreWords += " " + words;
+   }
+   return moreWords;
+});
+console.table(nameStudents);
