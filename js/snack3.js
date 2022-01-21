b4138eb93a0d80692e0cf935d90b872c76624638
vrossi96 <vrossi396@yahoo.it>
2022-01-21 17:56:39 +0100

    add files

---
+++ js/snack3.js
@@ -0,0 +1,5 @@
+/* SNACK 3
+Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+Usiamo i nuovi metodi degli array visti oggi in classe.
+*/
